afa66a42d9126f015c0e1533be260e2a642dd422
umaid-git <uxa180002@utdallas.edu>
2023-02-02 20:35:25 -0600

    Added functionality to perform statistical operations
    Added functions to find  the sum, mean, median and range of a numeric vector

--- Portfolio Component 1 Data Exploration/main.cpp
+++ Portfolio Component 1 Data Exploration/main.cpp
@@ -1,48 +1,127 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <execution>
+#include <cmath>
 
 using namespace std;
 
-int main(int argc, char** argv) {
-    
+ifstream readFile(string fileName){
     ifstream inputFileStream;
-    string lineFromFile;
-    string rm_in, medv_in;
-    const int MAX_LEN = 1000;
-    vector <double> rm(MAX_LEN);
-    vector <double> medv(MAX_LEN);
 
-    cout <<"Opening Boston.csv file" << endl;
-
-    inputFileStream.open("Boston.csv");
+    cout <<"Opening file" << fileName << endl;
+    inputFileStream.open(fileName);
 
     if(!inputFileStream.is_open()) {
         cout << "Could not open file Boston.csv" << endl;
-        return -1;
     }
 
+    return inputFileStream;
+}
+
+// this function returns the sum of all the values in a numeric vector
+double sumOfNumericVector(vector <double> vectorVariableName){
+    double sum=0;
+
+    // loops through the whole array and add the value
+    for(int i=0; i<vectorVariableName.size(); i++){
+        sum += vectorVariableName.at(i);
+    }
+
+    return sum;
+}
+
+// this function returns the mean of a numeric vector
+double meanOfNumericVector(vector <double> vectorVariableName){
+    return sumOfNumericVector(vectorVariableName)/vectorVariableName.size();
+}
+
+// this function returns the median of a numeric vector
+double medianOfNumericVector(vector <double> vectorVariableName){
+    bool even = false;
+
+    // checks if the vector is even or odd. 
+    if(vectorVariableName.size() % 2 == 0)
+        even = true;
+
+    sort(execution::par, vectorVariableName.begin(), vectorVariableName.end());
+    int middleIndex = vectorVariableName.size() / 2;
+
+    // if the vector size is even then get the left index and right index of the middle value 
+    if(even){
+        int leftIndex = middleIndex;
+        int rigtIndex = middleIndex+1;
+
+        double median = (vectorVariableName.at(leftIndex) + vectorVariableName.at(rigtIndex))/2;    // stores the average of left and right value of middle index
+
+        return median;
+    }
+    else {
+        return vectorVariableName.at(ceil(middleIndex));
+    }
+}
+
+double rangeOfNumericVector(vector <double> vectorVariableName){
+    sort(execution::par, vectorVariableName.begin(), vectorVariableName.end());
+    return vectorVariableName.at(vectorVariableName.size()-1)-vectorVariableName.at(0);
+}
+
+
+int main(int argc, char** argv) {
+    
+    // variable declaration
+    ifstream inputFileStream;
+    string lineFromFile;
+    string rm_feature_string_input_from_file, medv_feature_string_input_from_file;
+    const int MAX_LEN = 1000;
+    vector <double> rm_feature_vector(MAX_LEN);
+    vector <double> medv_feature_vector(MAX_LEN);
+
+    inputFileStream = readFile("Boston.csv");
+
+    // reads and display the features/ column name
     cout << "Reading line1 " << endl;
     getline(inputFileStream, lineFromFile);
+    cout << " This is the line from file " << lineFromFile << endl;
 
-    int numOfObs = 0;
+    int numOfObservation = 0;
 
     while(inputFileStream.good()){
 
-        getline(inputFileStream, rm_in, ',');
-        getline(inputFileStream, medv_in, '\n');
+        getline(inputFileStream, rm_feature_string_input_from_file, ',');
+        getline(inputFileStream, medv_feature_string_input_from_file, '\n');
 
-        rm.at(numOfObs) = stof(rm_in);
-        medv.at(numOfObs) = stof(medv_in);
+        // convert the string into a float and store into respective vectors
+        rm_feature_vector.at(numOfObservation) = stof(rm_feature_string_input_from_file);
+        medv_feature_vector.at(numOfObservation) = stof(medv_feature_string_input_from_file);
 
-        cout << "rm value of Obs *" << numOfObs << "* is ==" << rm.at(numOfObs) << endl;
-        cout << "medv value of Obs *" << numOfObs << "* is ==" << medv.at(numOfObs) << endl;
-        cout << "=============================================================== \n";
+        // cout << "rm value of Obs *" << numOfObservation << "* is ==" << rm_feature_vector.at(numOfObservation) << endl;
+        // cout << "medv value of Obs *" << numOfObservation << "* is ==" << medv_feature_vector.at(numOfObservation) << endl;
+        // cout << "=============================================================== \n";
 
-        numOfObs++;
+        numOfObservation++;
     }
 
-    cout << " This is the line from file " << lineFromFile << endl;
+    rm_feature_vector.resize(numOfObservation);
+    medv_feature_vector.resize(numOfObservation);
+
+    // closing the file
+    cout << "Closing the file" << endl;
+    inputFileStream.close();
+
+    // cout << "The sum of rm is == " << sumOfNumericVector(rm_feature_vector) << endl;
+    // cout << "The sum of medv is == " << sumOfNumericVector(medv_feature_vector) << endl;
+    
+    // cout << "The mean of rm is == " << meanOfNumericVector(rm_feature_vector) << endl;
+    // cout << "The mean of medv is == " << meanOfNumericVector(medv_feature_vector) << endl;
+
+    // cout << "The median of rm is == " << medianOfNumericVector(rm_feature_vector) << endl;
+    // cout << "The median of medv is == " << medianOfNumericVector(medv_feature_vector) << endl;
+
+    // cout << "The range of rm is == " << rangeOfNumericVector(rm_feature_vector) << endl;
+    // cout << "The range of medv is == " << rangeOfNumericVector(medv_feature_vector) << endl;
+
 
     return 0;
 }
